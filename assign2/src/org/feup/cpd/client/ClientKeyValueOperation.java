3b6d53f18a73a7a274a6c91ba8d50ee878ee4ddc
Miguel Rodrigues <up201906042@edu.fe.up.pt>
2022-05-12 19:02:56 +0100

    TestClient: code refactoring to accommodate the required interfaces

---
+++ assign2/src/org/feup/cpd/client/ClientKeyValueOperation.java
@@ -0,0 +1,57 @@
+package org.feup.cpd.client;
+
+import org.feup.cpd.interfaces.KeyValue;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class ClientKeyValueOperation implements KeyValue {
+
+    public ClientKeyValueOperation() {}
+
+    public String putClientSetup(String nodeAccessPoint, String fileName) {
+        StringBuilder keyToBigEndian = new StringBuilder(64);
+        String value = "";
+
+        try {
+            Path filePath = Path.of(fileName);
+            value = Files.readString(filePath);
+
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = messageDigest.digest(value.getBytes(StandardCharsets.UTF_8));
+
+            for (byte b : hash)
+                keyToBigEndian.append(String.format("%02x", b));
+
+        } catch (NoSuchAlgorithmException | IOException e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        String key = keyToBigEndian.toString();
+        put(key, value);
+
+        return key;
+    }
+
+    @Override
+    public void put(String key, String value) {
+        // TODO: make the communication with Node
+    }
+
+    @Override
+    public File get(String key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void delete(String key) {
+        throw new UnsupportedOperationException();
+    }
+}
--- assign2/src/org/feup/cpd/client/TestClient.java
+++ assign2/src/org/feup/cpd/client/TestClient.java
@@ -1,25 +1,74 @@
 package org.feup.cpd.client;
 
-import java.io.IOException;
+import org.feup.cpd.interfaces.Membership;
+
+import java.io.File;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 public class TestClient {
 
     public static void main(String[] args) {
-        Operation operation = new Operation();
-        String operationString = args[0];
+        if (args.length < 2) {
+            System.err.println("Usage: java TestClient <node_ap> <operation> [<opnd>]");
+            System.exit(1);
+        }
+
+        String nodeAccessPoint = args[0];
+        String operation = args[1];
+
+        switch (operation) {
+            case "join", "leave" -> handleMembershipOperation(nodeAccessPoint, operation);
+            case "put", "get", "delete" -> {
+                String operationArgument = args[2];
+                handleKeyValueOperation(nodeAccessPoint, operation, operationArgument);
+            }
+            default -> throw new IllegalArgumentException("Unexpected operation: " + operation);
+        }
+    }
+
+    // FIXME: I don't have any idea about what should this method return - Mike
+    private static void handleMembershipOperation(String nodeAccessPoint, String operation) {
+        String remoteCallReturn = "";
 
         try {
-            switch (operationString) {
-                case "join" -> System.err.println("JOIN Not Yet Implemented");
-                case "leave" -> System.err.println("LEAVE Not Yet Implemented");
-                case "put" -> System.out.println(operation.put(args[1]));
-                case "get" -> System.err.println("GET Not Yet Implemented");
-                case "delete" -> System.err.println("DELETE Not Yet Implemented");
-            }
+            Registry registry = LocateRegistry.getRegistry(nodeAccessPoint);
+            Membership remoteCall = (Membership) registry.lookup("Membership");
 
-        } catch (IOException | RuntimeException ioException) {
-            System.err.println(ioException.getMessage());
-            ioException.printStackTrace();
+            if (operation.equals("join"))
+                remoteCallReturn = remoteCall.join();
+            else if (operation.equals("leave"))
+                remoteCallReturn = remoteCall.leave();
+
+        } catch (RemoteException | NotBoundException e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        // FIXME: printing for now, this should (must) be changed later, idk for what - Mike
+        System.out.println(remoteCallReturn);
+    }
+
+    private static void handleKeyValueOperation(String nodeAccessPoint, String operation, String argument) throws IllegalArgumentException {
+        ClientKeyValueOperation keyValueOperation = new ClientKeyValueOperation();
+
+        switch (operation) {
+            case "put" -> {
+                String key = keyValueOperation.putClientSetup(nodeAccessPoint, argument);
+                System.out.println("File saved with the following key: " + key);
+            }
+            case "get" -> {
+                File file = keyValueOperation.get(argument);
+                System.out.println("File saved to the following location: " + file.getPath());
+            }
+            case "delete" -> {
+                keyValueOperation.delete(argument);
+                System.out.println("Erased the following key: " + argument);
+            }
+            default -> throw new IllegalArgumentException("Unexpected argument: " + argument);
         }
     }
 }
